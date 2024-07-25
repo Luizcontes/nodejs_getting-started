f50508638a993c3a5ea6f4605e7d7e6707361583
Luiz Contes <luiz.contes@hotmail.com>
2024-07-25 21:13:23 +0100

    installing jest for testing creation

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import { isAdult } from '../src/type_script_example';
-import { User } from '../src/type_script_example';
+// import { isAdult } from '../src/type_script_example';
+// import { User } from '../src/type_script_example';
 
-const justine: User = {
-  name: 'Justine',
-  age: 23,
-};
+// const justine: User = {
+//   name: 'Justine',
+//   age: 23,
+// };
 
-const isJustineAnAdult: boolean = isAdult( justine );
+// const isJustineAnAdult: boolean = isAdult( justine );
 
-console.log(`Is ${justine.name} an adult: ${isJustineAnAdult}`);
+// console.log(`Is ${justine.name} an adult: ${isJustineAnAdult}`);
+
+import sum from './sum.ts';
+
+console.log(sum(2, 2));
---
+++ tests/sum.test.ts
@@ -0,0 +1,5 @@
+import sum from '../src/sum.ts';
+
+test('adds two numbers', () => {
+    expect(sum(2, 2)).toBe(4);
+});
