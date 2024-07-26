a62bdcf42d78d86c7768e5777a1bf652d405b38b
Luiz <luiz.contes@hotmail.com>
2024-07-26 09:15:42 +0100

    first test created, coverage metrics

---
+++ README.md
@@ -0,0 +1,3 @@
+Testable Javascript
+
+25/jul/2024 -> pag. 23
--- tests/sum.test.ts
+++ tests/sum.test.ts
@@ -3,3 +3,7 @@ import sum from '../src/sum.ts';
 test('adds two numbers', () => {
     expect(sum(2, 2)).toBe(4);
 });
+
+test('adds two numbers', () => {
+    expect(sum(1, 2)).toBeGreaterThan(0);
+});
